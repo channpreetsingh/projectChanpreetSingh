6a57208bf33d2234be82c2d41e7380387a392310
channpreetsingh <chanpreetsingh051@gmail.com>
2026-06-01 13:08:10 -0400

    first commit for ICE 4 ..

---
+++ src/main/java/com/mycompany/project_chanpreet_singh/Project_Chanpreet_Singh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_chanpreet_singh;
+
+/**
+ *
+ * @author chanp
+ */
+public class Project_Chanpreet_Singh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
